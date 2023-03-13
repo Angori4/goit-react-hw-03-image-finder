15275668d6c83c41e5bff08bb1466135978a6493
Angori4 <110497521+Angori4@users.noreply.github.com>
2023-03-13 12:05:24 +0200

    fix

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,6 +1,6 @@
 export const fetchImage = (imgName, page = 1) => {
   return fetch(
-    `https://pixabay.com/api/?key=33044994-c2ced58164f7879a8375986b7=${imgName}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12`
+    `https://pixabay.com/api/?key=32945062-68d6d40c8d925b295193ffb26&q=${imgName}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12`
   ).then(response => {
     if (response.ok) {
       return response.json();
